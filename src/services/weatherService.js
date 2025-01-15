6880f2c319a0883e74fb2b868f1dea0a69745a27
JesseMashoana <jesse.mashoana@gmail.com>
2025-01-15 09:48:25 +0200

    Update weatherService.js

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const WEATHER_API_KEY = 'b90b06fece2d43efa8915501251501'
+const WEATHER_API_KEY = 'your free weather api key'
 const BASE_URL = 'https://api.weatherapi.com/v1';
 
 export const getWeatherData = async (location) => {
